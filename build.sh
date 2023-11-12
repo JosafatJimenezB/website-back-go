a15a71e0fdeea1091aab30ebd3063daeccc2e626
josafat <josa221001@gmail.com>
2023-11-11 21:58:57 -0600

    add: archivos de deploy

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 # GOBIN=$(pwd)/functions go install ./...
 # Dentro de build.sh
-go build -o main
+mkdir -p "$(pwd)/functions"
+GOBIN=$(pwd)/functions go install ./...
+chmod +x "$(pwd)"/functions/*
+go env
